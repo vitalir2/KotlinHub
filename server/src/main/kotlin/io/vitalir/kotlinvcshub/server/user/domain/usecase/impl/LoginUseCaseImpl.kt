69f2bc1e7892061da7f8e5c60aeb4dc6e8b679b6
vitalir2 <vitalyi100@mail.ru>
2022-10-24 17:43:12 +0300

    Fix the first test and refactor code

--- server/src/main/kotlin/io/vitalir/kotlinvcshub/server/user/domain/usecase/impl/LoginUseCaseImpl.kt
+++ server/src/main/kotlin/io/vitalir/kotlinvcshub/server/user/domain/usecase/impl/LoginUseCaseImpl.kt
@@ -2,12 +2,12 @@ package io.vitalir.kotlinvcshub.server.user.domain.usecase.impl
 
 import arrow.core.Either
 import arrow.core.continuations.either
-import arrow.core.left
-import arrow.core.right
-import io.vitalir.kotlinvcshub.server.user.domain.usecase.LoginUseCase
 import io.vitalir.kotlinvcshub.server.user.domain.model.User
 import io.vitalir.kotlinvcshub.server.user.domain.model.UserError
 import io.vitalir.kotlinvcshub.server.user.domain.persistence.UserPersistence
+import io.vitalir.kotlinvcshub.server.user.domain.usecase.LoginUseCase
+import io.vitalir.kotlinvcshub.server.user.domain.validation.EmailValidationRule
+import io.vitalir.kotlinvcshub.server.user.domain.validation.LoginValidationRule
 
 internal class LoginUseCaseImpl(
     private val userPersistence: UserPersistence,
@@ -27,25 +27,8 @@ internal class LoginUseCaseImpl(
 
     private fun validateIdentifier(identifier: User.Credentials.Identifier): Either<UserError.ValidationFailed, Unit> {
         return when (identifier) {
-            is User.Credentials.Identifier.Email -> validateEmail(identifier)
-            is User.Credentials.Identifier.Login -> validateLogin(identifier)
-        }
-    }
-
-    private fun validateEmail(email: User.Credentials.Identifier.Email): Either<UserError.ValidationFailed, Unit> {
-        val emailRegex = EMAIL_REGEX
-        return if (emailRegex.matches(email.value)) {
-            Unit.right()
-        } else {
-            UserError.ValidationFailed.left()
-        }
-    }
-
-    private fun validateLogin(login: User.Credentials.Identifier.Login): Either<UserError.ValidationFailed, Unit> {
-        return if (login.value.length in LOGIN_LENGTH_RANGE) {
-            Unit.right()
-        } else {
-            UserError.ValidationFailed.left()
+            is User.Credentials.Identifier.Email -> EmailValidationRule.validate(identifier)
+            is User.Credentials.Identifier.Login -> LoginValidationRule.validate(identifier)
         }
     }
 
@@ -55,13 +38,4 @@ internal class LoginUseCaseImpl(
         hashedUserPassword: String
     ): Either<UserError.InvalidCredentials, Unit> =
         Either.Right(Unit)
-
-    companion object {
-        private const val LOGIN_MIN_LENGTH = 6
-        private const val LOGIN_MAX_LENGTH = 20
-
-        // TODO: Add real regex
-        private val EMAIL_REGEX = Regex("TODO")
-        private val LOGIN_LENGTH_RANGE = LOGIN_MIN_LENGTH..LOGIN_MAX_LENGTH
-    }
 }
---
+++ server/src/main/kotlin/io/vitalir/kotlinvcshub/server/user/domain/validation/EmailValidationRule.kt
@@ -0,0 +1,21 @@
+package io.vitalir.kotlinvcshub.server.user.domain.validation
+
+import arrow.core.Either
+import arrow.core.left
+import arrow.core.right
+import io.vitalir.kotlinvcshub.server.user.domain.model.User
+import io.vitalir.kotlinvcshub.server.user.domain.model.UserError
+
+internal object EmailValidationRule : UserValidationRule<User.Credentials.Identifier.Email> {
+
+    override fun validate(entity: User.Credentials.Identifier.Email): Either<UserError.ValidationFailed, Unit> {
+        return if (EMAIL_REGEX.matches(entity.value)) {
+            Unit.right()
+        } else {
+            UserError.ValidationFailed.left()
+        }
+    }
+
+    // TODO: Add real regex
+    private val EMAIL_REGEX = Regex("TODO")
+}
