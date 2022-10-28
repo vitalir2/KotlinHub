d18247f21a8767c05097ffcf00d6dfd2e777bbf1
vitalir2 <vitalyi100@mail.ru>
2022-10-28 18:04:23 +0300

    Add another test for checking if user exists

--- server/src/main/kotlin/io/vitalir/kotlinvcshub/server/repository/domain/usecases/impl/CreateRepositoryUseCaseImpl.kt
+++ server/src/main/kotlin/io/vitalir/kotlinvcshub/server/repository/domain/usecases/impl/CreateRepositoryUseCaseImpl.kt
@@ -1,14 +1,22 @@
 package io.vitalir.kotlinvcshub.server.repository.domain.usecases.impl
 
 import arrow.core.Either
+import arrow.core.left
 import arrow.core.right
 import io.vitalir.kotlinvcshub.server.repository.domain.CreateRepositoryData
 import io.vitalir.kotlinvcshub.server.repository.domain.RepositoryError
 import io.vitalir.kotlinvcshub.server.repository.domain.usecases.CreateRepositoryUseCase
+import io.vitalir.kotlinvcshub.server.user.domain.persistence.UserPersistence
 
-internal class CreateRepositoryUseCaseImpl : CreateRepositoryUseCase {
+internal class CreateRepositoryUseCaseImpl(
+    private val userPersistence: UserPersistence,
+) : CreateRepositoryUseCase {
 
     override suspend fun invoke(initData: CreateRepositoryData): Either<RepositoryError.Create, Unit> {
-        return Unit.right()
+        return if (userPersistence.isUserExists(initData.userId)) {
+            Unit.right()
+        } else {
+            RepositoryError.Create.InvalidUserId.left()
+        }
     }
 }
--- server/src/test/kotlin/io/vitalir/kotlinvschub/server/repository/domain/CreateRepositoryUseCaseSpec.kt
+++ server/src/test/kotlin/io/vitalir/kotlinvschub/server/repository/domain/CreateRepositoryUseCaseSpec.kt
@@ -3,25 +3,36 @@ package io.vitalir.kotlinvschub.server.repository.domain
 import io.kotest.assertions.arrow.core.shouldBeLeft
 import io.kotest.assertions.arrow.core.shouldBeRight
 import io.kotest.core.spec.style.ShouldSpec
+import io.mockk.coEvery
+import io.mockk.mockk
 import io.vitalir.kotlinvcshub.server.repository.domain.CreateRepositoryData
 import io.vitalir.kotlinvcshub.server.repository.domain.Repository
 import io.vitalir.kotlinvcshub.server.repository.domain.RepositoryError
 import io.vitalir.kotlinvcshub.server.repository.domain.usecases.CreateRepositoryUseCase
 import io.vitalir.kotlinvcshub.server.repository.domain.usecases.impl.CreateRepositoryUseCaseImpl
+import io.vitalir.kotlinvcshub.server.user.domain.persistence.UserPersistence
 
 class CreateRepositoryUseCaseSpec : ShouldSpec() {
 
     private lateinit var createRepositoryUseCase: CreateRepositoryUseCase
 
+    private lateinit var userPersistence: UserPersistence
+
     init {
         beforeEach {
-            createRepositoryUseCase = CreateRepositoryUseCaseImpl()
+            userPersistence = mockk()
+            createRepositoryUseCase = CreateRepositoryUseCaseImpl(
+                userPersistence = userPersistence,
+            )
         }
 
         should("return success if data is valid") {
+            val userId = 123
+            coEvery { userPersistence.isUserExists(userId) } returns true
+
             val result = createRepositoryUseCase(
                 CreateRepositoryData(
-                    userId = 123,
+                    userId = userId,
                     name = "any",
                     accessMode = Repository.AccessMode.PUBLIC,
                 )
@@ -32,6 +43,8 @@ class CreateRepositoryUseCaseSpec : ShouldSpec() {
 
         should("return error if user does not exist") {
             val notExistingUserId = 5
+            coEvery { userPersistence.isUserExists(notExistingUserId) } returns false
+
             val result = createRepositoryUseCase(
                 CreateRepositoryData(
                     userId = notExistingUserId,
