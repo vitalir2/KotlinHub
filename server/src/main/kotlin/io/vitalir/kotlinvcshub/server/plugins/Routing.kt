4232f288c0438eb2ba127ec3b09b8f27e7da96ab
vitalir2 <vitalyi100@mail.ru>
2022-10-29 12:35:41 +0300

    Install Call logging plugin

--- server/src/main/kotlin/io/vitalir/kotlinvcshub/server/plugins/Routing.kt
+++ server/src/main/kotlin/io/vitalir/kotlinvcshub/server/plugins/Routing.kt
@@ -1,21 +1,14 @@
 package io.vitalir.kotlinvcshub.server.plugins
 
 import io.ktor.server.application.*
-import io.ktor.server.plugins.statuspages.*
+import io.ktor.server.plugins.callloging.*
 import io.ktor.server.routing.*
-import io.ktor.server.webjars.*
 import io.vitalir.kotlinvcshub.server.infrastructure.di.AppGraph
 import io.vitalir.kotlinvcshub.server.repository.routing.repositoryRoutes
 import io.vitalir.kotlinvcshub.server.user.routes.userRoutes
 
 fun Application.configureRouting(appGraph: AppGraph) {
-
-    install(StatusPages) {
-
-    }
-    install(Webjars) {
-        path = "/webjars" //defaults to /webjars
-    }
+    install(CallLogging)
     routing {
         userRoutes(
             jwtConfig = appGraph.appConfig.jwt,
