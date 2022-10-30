bb3311556e6374090b3f43e810f00e69e51bd509
vitalir2 <vitalyi100@mail.ru>
2022-10-30 14:02:08 +0300

    Fix creation git directory

--- server/src/main/kotlin/io/vitalir/kotlinvcshub/server/infrastructure/git/GitPlugin.kt
+++ server/src/main/kotlin/io/vitalir/kotlinvcshub/server/infrastructure/git/GitPlugin.kt
@@ -3,14 +3,15 @@ package io.vitalir.kotlinvcshub.server.infrastructure.git
 import io.ktor.server.application.*
 import io.ktor.server.application.hooks.*
 import kotlin.io.path.Path
-import kotlin.io.path.createDirectory
+import kotlin.io.path.createDirectories
 import kotlin.io.path.isDirectory
 
 internal val GitPlugin = createApplicationPlugin("kotlinHubGit") {
-    on(MonitoringEvent(ApplicationStarted)) {
+    on(MonitoringEvent(ApplicationStarted)) { application ->
         val repositoriesPath = Path(GitConstants.REPOSITORIES_PATH)
         if (!repositoriesPath.isDirectory()) {
-            repositoriesPath.createDirectory()
+            application.log.info("Creating base git repos dir")
+            repositoriesPath.createDirectories()
         }
     }
 }
