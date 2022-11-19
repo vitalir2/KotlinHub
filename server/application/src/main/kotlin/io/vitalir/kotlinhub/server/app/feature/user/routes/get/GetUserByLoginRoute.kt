a4f85e49637e02c0464cbd565fcad77352cf1434
Vitaly Khvostov <vitalyi100@mail.ru>
2022-11-19 14:24:04 +0300

    Get user by any identifier route (#22)

--- server/application/src/main/kotlin/io/vitalir/kotlinhub/server/app/feature/user/routes/get/GetUserByLoginRoute.kt
+++ server/application/src/main/kotlin/io/vitalir/kotlinhub/server/app/feature/user/routes/get/GetUserByLoginRoute.kt
@@ -1,40 +1,63 @@
 package io.vitalir.kotlinhub.server.app.feature.user.routes.get
 
+import arrow.core.Either
+import arrow.core.left
+import arrow.core.right
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
 import io.vitalir.kotlinhub.server.app.common.routes.ResponseData
 import io.vitalir.kotlinhub.server.app.common.routes.extensions.respondWith
+import io.vitalir.kotlinhub.server.app.feature.user.domain.model.User
 import io.vitalir.kotlinhub.server.app.feature.user.domain.model.UserIdentifier
 import io.vitalir.kotlinhub.server.app.feature.user.domain.usecase.GetUserByIdentifierUseCase
 import io.vitalir.kotlinhub.server.app.feature.user.routes.common.extensions.asApiUser
+import io.vitalir.kotlinhub.server.app.infrastructure.auth.requireParameter
 
 internal fun Route.getUserByUsernameRoute(
     getUserByIdentifierUseCase: GetUserByIdentifierUseCase,
 ) {
-    get("{login}/") {
-        val login = call.parameters["login"] ?: run {
-            call.respondWith(
-                ResponseData.fromErrorData(
-                    code = HttpStatusCode.BadRequest,
-                    errorMessage = "invalid login",
-                )
-            )
+    get("{identifier}") {
+        val identifierFromPath = call.requireParameter("identifier")
+        val identifierType = call.request.queryParameters["type"]
+        val parseResult = parseIdentifier(identifierFromPath = identifierFromPath, type = identifierType)
+        if (parseResult is Either.Left) {
+            call.respondWith(parseResult.value)
             return@get
         }
 
-        val user = getUserByIdentifierUseCase(UserIdentifier.Username(login))
-        val resultResponseData = if (user != null) {
-            ResponseData(
-                code = HttpStatusCode.OK,
-                body = GetUserByLoginResponse(user = user.asApiUser),
-            )
-        } else {
-            ResponseData.fromErrorData(
-                code = HttpStatusCode.NotFound,
-                errorMessage = "user not found",
-            )
-        }
-        call.respondWith(resultResponseData)
+        // Cannot fail
+        val identifier = (parseResult as Either.Right).value
+        val user = getUserByIdentifierUseCase(identifier)
+
+        call.respondWith(user.toResponseData())
+    }
+}
+
+private fun parseIdentifier(
+    identifierFromPath: String,
+    type: String?,
+): Either<ResponseData, UserIdentifier> {
+    return when (type) {
+        // Default - username
+        null -> UserIdentifier.Username(identifierFromPath).right()
+        "id" -> UserIdentifier.Id(identifierFromPath.toInt()).right()
+        "username" -> UserIdentifier.Username(identifierFromPath).right()
+        "email" -> UserIdentifier.Email(identifierFromPath).right()
+        else -> ResponseData.badRequest("invalid identifier type=$type").left()
+    }
+}
+
+private fun User?.toResponseData(): ResponseData {
+    return if (this != null) {
+        ResponseData(
+            code = HttpStatusCode.OK,
+            body = GetUserByLoginResponse(user = asApiUser),
+        )
+    } else {
+        ResponseData.fromErrorData(
+            code = HttpStatusCode.NotFound,
+            errorMessage = "user not found",
+        )
     }
 }
