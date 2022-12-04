77dc23307a46ca4157c3239c1d17fbc07e78657a
vitalir2 <vitalyi100@mail.ru>
2022-12-04 16:28:57 +0300

    Add first page

---
+++ web/src/features/main/views/MainPage.tsx
@@ -0,0 +1,23 @@
+function Profile() {
+    return (
+        <div/>
+    )
+}
+
+function MainContent() {
+    return (
+        <div/>
+    )
+}
+
+function MainPage() {
+    return (
+        <div>
+            <Profile/>
+            <MainContent/>
+            <p>Hello world!</p>
+        </div>
+    )
+}
+
+export default MainPage;
