c190b43bf700ed57ed935c8247e2b6895e535ef8
vitalir2 <vitalyi100@mail.ru>
2022-12-04 17:46:20 +0300

    Design main component

--- web/src/features/main/views/MainPage.tsx
+++ web/src/features/main/views/MainPage.tsx
@@ -9,15 +9,74 @@ function Profile() {
 }
 
 function MainContent() {
+    const repositories = [
+        {
+            name: "KotlinHub",
+            accessMode: RepositoryAccessMode.PUBLIC,
+            updatedAt: new Date().toDateString(),
+            description: "Version control hosting written in Kotlin",
+        },
+        {
+            name: "React",
+            accessMode: RepositoryAccessMode.PUBLIC,
+            updatedAt: new Date().toDateString(),
+            description: "A declarative, efficient, and flexible JavaScript library for building user interfaces",
+        },
+    ]
     return (
-        <main/>
+        <main className="flex flex-col p-4">
+            <RepositoriesView repositories={repositories}/>
+        </main>
+    )
+}
+
+type RepositoriesViewProps = {
+    repositories: Repository[],
+}
+
+type Repository = {
+    name: string,
+    accessMode: RepositoryAccessMode,
+    updatedAt: string,
+    description?: string,
+}
+
+enum RepositoryAccessMode {
+    PUBLIC = "Public",
+    PRIVATE = "Private",
+}
+
+function RepositoriesView(props: RepositoriesViewProps) {
+    const {repositories} = props
+    return (
+        <div className="flex flex-col gap-4">
+            {repositories.map((repository) => <RepositoryView repository={repository} />)}
+        </div>
+    )
+}
+
+type RepositoryViewProps = {
+    repository: Repository,
+}
+function RepositoryView(props: RepositoryViewProps) {
+    const {repository} = props
+    return (
+        <div className="flex flex-col">
+            <div className="flex flex-row gap-1">
+                <a className="text-blue-700 font-semibold text-lg" rel="#">{repository.name}</a>
+                <span className="border-2 border-slate-50 font-bold">{repository.accessMode}</span>
+            </div>
+            <p className="text-xs font-light text-start">{repository.description}</p>
+            <p className="text-xs font-light text-start mt-1">{"updated " + repository.updatedAt}</p>
+        </div>
     )
 }
 
 function MainPage() {
     return (
-        <div className="flex flex-row">
+        <div className="flex flex-row justify-center gap-2">
             <Profile/>
+            <div className="border-r"/>
             <MainContent/>
         </div>
     )
