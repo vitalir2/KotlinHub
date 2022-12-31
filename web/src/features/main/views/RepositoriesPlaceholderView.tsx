7219c87cca7cfbf60921d5495ae521d996b91e51
vitalir2 <vitalyi100@mail.ru>
2022-12-31 10:59:49 +0300

    Remove web module

--- web/src/features/main/views/RepositoriesPlaceholderView.tsx
+++
@@ -1,8 +0,0 @@
-export function RepositoriesPlaceholderView() {
-    return (
-        <>
-            <h2 className="">No repositories yet</h2>
-            <p><a href="#">Add</a> a new one or <a href="#">watch</a> other repositories</p>
-        </>
-    );
-}
