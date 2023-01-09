27a4d60f3572a7772ee611d895fe6692c5c2ea0e
vitalir2 <vitalyi100@mail.ru>
2023-01-09 17:57:56 +0300

    Remove old web

--- web/src/jsMain/kotlin/io/vitalir/web/common/NetworkConstants.kt
+++
@@ -1,6 +0,0 @@
-package io.vitalir.web.common
-
-object NetworkConstants {
-
-    const val BASE_PATH = "http://localhost/api/v1"
-}
--- web/src/jsMain/kotlin/io/vitalir/web/network/HttpClient.kt
+++
@@ -1,5 +0,0 @@
-package io.vitalir.web.network
-
-import io.vitalir.kotlinhub.shared.common.createHttpClient
-
-val baseHttpClient = createHttpClient()
