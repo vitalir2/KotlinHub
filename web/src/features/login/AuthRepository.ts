a0ce089af69272b9f8ef0cea2557130614ada4a9
vitalir2 <vitalyi100@mail.ru>
2023-01-19 08:52:34 +0300

    Add the main logic for the page

---
+++ web/src/features/login/AuthRepository.ts
@@ -0,0 +1,31 @@
+import {baseApi} from "../../app/fetch";
+
+export interface LoginUserRequest {
+    login: string,
+    password: string,
+}
+ interface LoginUserResponse {
+    token: string,
+}
+
+export interface LoginUserResult {
+    token: string,
+}
+
+export interface AuthRepository {
+    loginUser(request: LoginUserRequest): Promise<LoginUserResult>
+}
+
+export class DefaultAuthRepository implements AuthRepository {
+    loginUser(request: LoginUserRequest): Promise<LoginUserResult> {
+        return baseApi.post<LoginUserResponse>("/users/auth", request)
+            .then(response => response.data)
+            .then(body => this.mapResponseToResult(body))
+    }
+
+    private mapResponseToResult(response: LoginUserResponse): LoginUserResult {
+        return {
+            token: response.token,
+        }
+    }
+}
--- web/src/features/login/LoginPage.tsx
+++ web/src/features/login/LoginPage.tsx
@@ -1,6 +1,6 @@
 import {
     Button,
-    Checkbox,
+    Checkbox, CircularProgress,
     Container,
     FormControlLabel,
     Link,
@@ -10,32 +10,21 @@ import {
     Typography
 } from "@mui/material";
 import React from "react";
+import {useAppDispatch, useAppSelector} from "../../app/hooks";
+import {loginUser, setLogin, setPassword, setRememberMe} from "./LoginSlice";
+import {AppDispatch} from "../../app/store";
 
 export function LoginPage() {
+    const containerStyle: SxProps<Theme> = {
+        display: "flex",
+        flexDirection: "column",
+        height: "100vh",
+        justifyContent: "center",
+    }
+
     return (
-        <Container maxWidth={"xs"} sx={{
-            display: "flex",
-            flexDirection: "column",
-            height: "100vh",
-            justifyContent: "center",
-        }}>
-            <Stack spacing={1}>
-                <Typography variant={"h3"} textAlign={"center"}>
-                    Login
-                </Typography>
-                <TextField
-                    variant={"outlined"}
-                    label={"Username or email"}
-                />
-                <TextField
-                    variant={"outlined"}
-                    label={"Password"}
-                />
-                <AdditionalOptions/>
-                <Button variant={"contained"}>
-                    Sign in
-                </Button>
-            </Stack>
+        <Container maxWidth={"xs"} sx={containerStyle}>
+            <LoginForm/>
             <RegisterNow sx={{
                 marginTop: 8,
             }}/>
@@ -43,10 +32,71 @@ export function LoginPage() {
     )
 }
 
-function AdditionalOptions() {
+function LoginForm() {
+    const state = useAppSelector(state => state.login)
+    const dispatch = useAppDispatch()
+
+    const isButtonEnabled = state.login.errorMessage === undefined && state.password.errorMessage === undefined &&
+        state.login.value !== "" && state.password.value !== ""
+
+    return (
+        <Stack spacing={1}>
+            <Typography variant={"h3"} textAlign={"center"}>
+                Login
+            </Typography>
+            <TextField
+                variant={"outlined"}
+                label={"Username or email"}
+                error={state.login.errorMessage !== undefined}
+                helperText={state.login.errorMessage}
+                onChange={event => dispatch(setLogin(event.target.value))}
+            />
+            <TextField
+                variant={"outlined"}
+                label={"Password"}
+                type={"password"}
+                error={state.password.errorMessage !== undefined}
+                helperText={state.password.errorMessage}
+                onChange={event => dispatch(setPassword(event.target.value))}
+            />
+            <AdditionalOptions dispatch={dispatch} rememberUser={state.rememberUser}/>
+            <Button
+                variant={"contained"}
+                disabled={!isButtonEnabled}
+                onClick={() => {
+                    if (!state.isValidating) {
+                        dispatch(
+                            loginUser(
+                                {
+                                    login: state.login.value,
+                                    password: state.password.value,
+                                }
+                            )
+                        )
+                    }
+                }}>
+                {state.isValidating ? <CircularProgress/> : "Sign in"}
+            </Button>
+        </Stack>
+    )
+}
+
+interface AdditionalOptionsProps {
+    dispatch: AppDispatch,
+    rememberUser: boolean,
+}
+
+function AdditionalOptions(props: AdditionalOptionsProps) {
+    const {dispatch, rememberUser} = props
+
     return (
         <Stack direction={"row"} justifyContent={"space-between"}>
-            <FormControlLabel control={<Checkbox/>} label={"Remember me"}/>
+            <FormControlLabel
+                control={<Checkbox/>}
+                label={"Remember me"}
+                checked={rememberUser}
+                onChange={(event, checked) => dispatch(setRememberMe(checked))}
+            />
             <Link href={"#"} underline={"none"} sx={{
                 maxWidth: "xs",
                 width: 100,
@@ -62,7 +112,7 @@ interface RegisterNowProps {
 }
 
 function RegisterNow(props: RegisterNowProps) {
-    const { sx } = props
+    const {sx} = props
     return (
         <Stack sx={sx}>
             <Typography variant={"body1"} sx={{
