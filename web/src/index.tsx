6b6c611d576417e4d50e7d441aa8d19c87e5a0a8
vitalir2 <vitalyi100@mail.ru>
2023-01-26 20:49:25 +0300

    Add navigation setup

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
+import {createAppRouter} from "./app/navigation";
+import {RouterProvider} from "react-router-dom";
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
+const router = createAppRouter()
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+      <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
