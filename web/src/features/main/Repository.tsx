c45607c4390cf59a0fe44ab798401e2ef56f67bf
vitalir2 <vitalyi100@mail.ru>
2023-02-12 17:43:35 +0300

    Add navigation to the repository page

--- web/src/features/main/Repository.tsx
+++ web/src/features/main/Repository.tsx
@@ -1,8 +1,13 @@
-import {Box, Chip, Stack, Typography} from "@mui/material";
+import {Box, Chip, Link, Stack, SxProps, Typography} from "@mui/material";
+import { Link as RouterLink } from "react-router-dom"
 import {Repository as RepositoryModel} from "../repositories/Repository";
 
 export interface RepositoryProps {
     repository: RepositoryModel,
+}
+
+const repositoryStyle: SxProps = {
+    textDecoration: "none",
 }
 
 export function Repository(props: RepositoryProps) {
@@ -11,7 +16,11 @@ export function Repository(props: RepositoryProps) {
     return (
         <Stack spacing={0.5}>
             <Stack spacing={1} direction={"row"}>
-                <Typography variant={"h6"}>
+                <Typography component={RouterLink}
+                            sx={repositoryStyle}
+                            variant={"h6"}
+                            to={`/repositories/${repository.name}`}
+                >
                     {repository.name}
                 </Typography>
                 <Chip label={repository.accessMode} variant={"outlined"} color={"secondary"} size={"small"}/>
