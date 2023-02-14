2802d87530b0750c09559924d7e3c6f4c20e6ece
Vitaly Khvostov <vitalyi100@mail.ru>
2023-02-14 21:43:31 +0300

    Add prod and dev environments for the frontend app (#47)

---
+++ web/src/app/environment.ts
@@ -0,0 +1,12 @@
+import * as process from "process";
+
+function isProductionEnv(): boolean {
+    return process.env.NODE_ENV === 'production'
+}
+
+export function isProductionBackend(): boolean {
+    return process.env.REACT_APP_BACKEND_ENVIRONMENT === 'production' || isProductionEnv()
+}
+
+export const BACKEND_URL_PRODUCTION = process.env.REACT_APP_BACKEND_URL_PRODUCTION
+export const BACKEND_URL_DEVELOPMENT = "localhost"
