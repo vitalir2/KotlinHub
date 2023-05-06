fe8a341d40120a85ba62d276f872a1abaf47bebb
vitalir2 <vitalyi100@mail.ru>
2023-05-06 18:34:59 +0300

    Add user settings page

---
+++ web/src/features/auth/settings/UserSettingsPage.tsx
@@ -0,0 +1,13 @@
+import {Stack, Typography} from "@mui/material";
+
+export function UserSettingsPage() {
+    return (
+        <Stack sx={{
+            p: 8,
+        }}>
+            <Typography variant={"h3"}>
+                Settings screen
+            </Typography>
+        </Stack>
+    );
+}
