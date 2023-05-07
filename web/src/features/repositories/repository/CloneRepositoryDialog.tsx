301a550e36dd1eaceefe8a016a4f7bc5ae49bc99
vitalir2 <vitalyi100@mail.ru>
2023-05-07 08:54:10 +0300

    Copy clone URL action

--- web/src/features/repositories/repository/CloneRepositoryDialog.tsx
+++ web/src/features/repositories/repository/CloneRepositoryDialog.tsx
@@ -1,5 +1,8 @@
 import {Repository} from "../Repository";
-import {Stack, SxProps, Typography} from "@mui/material";
+import {IconButton, Snackbar, Stack, SxProps, Typography} from "@mui/material";
+import {ContentCopy} from "@mui/icons-material";
+import {copyText} from "../../../core/utils/copyText";
+import {useState} from "react";
 
 const cloneRepositoryStyle: SxProps = {
     padding: "1rem",
@@ -9,18 +12,38 @@ const cloneRepositoryUrlInputStyle: SxProps = {
     borderRadius: 1,
     borderColor: "light-grey",
     backgroundColor: "#EEEEEE",
-    padding: "0.2rem",
+    p: 1,
 }
 
 export function CloneRepositoryDialog(repository: Repository) {
+    const [isSnackbarShown, setSnackbarShown] = useState(false);
+
     return (
         <Stack spacing={1} sx={cloneRepositoryStyle}>
             <Typography variant={"body1"}>
                 Clone your repository by HTTP
             </Typography>
-            <Typography variant={"body2"} sx={cloneRepositoryUrlInputStyle}>
-                {repository.cloneUrl}
-            </Typography>
+            <Stack direction={"row"}>
+                <Typography variant={"body2"} sx={cloneRepositoryUrlInputStyle}>
+                    {repository.cloneUrl}
+                </Typography>
+                <IconButton onClick={() => {
+                    copyText(
+                        repository.cloneUrl,
+                        () => setSnackbarShown(true),
+                    )
+                }}>
+                    <ContentCopy sx={{fontSize: 12}}/>
+                </IconButton>
+            </Stack>
+            <Snackbar
+                anchorOrigin={{vertical: "bottom", horizontal: "center"}}
+                open={isSnackbarShown}
+                onClose={() => setSnackbarShown(false)}
+                autoHideDuration={2000}
+                message={"Content was copied"}
+                key={"copy"}
+                />
         </Stack>
     );
 }
