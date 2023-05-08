5b0d478082f8a1764b40bcbf122131b5d99f165d
vitalir2 <vitalyi100@mail.ru>
2023-05-08 08:12:40 +0300

    Fix copying completely

--- web/src/core/utils/copyText.ts
+++ web/src/core/utils/copyText.ts
@@ -1,13 +1,19 @@
-// TODO: Replace by navigator API when https will be supported
+// TODO: Replace by navigator API when HTTPS will be supported
 export function copyText(
     text: string,
     onSuccess: () => void,
     onError: () => void = () => {},
 ) {
-    const input = document.createElement('input');
-    input.setAttribute('value', text);
-    document.body.appendChild(input);
-    input.select();
-    document.execCommand('copy');
-    document.body.removeChild(input);
+    let aux = document.createElement("div");
+    aux.setAttribute("contentEditable", "true");
+    aux.innerHTML = text;
+    document.body.appendChild(aux);
+    window.getSelection()?.selectAllChildren(aux);
+    const isSuccessful = document.execCommand("copy");
+    if (isSuccessful) {
+        onSuccess()
+    } else {
+        onError()
+    }
+    document.body.removeChild(aux);
 }
